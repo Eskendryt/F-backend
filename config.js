0b301cc0c481f65067cc45423aeb81d167f25f35
Eskndr <eskndrytbarek323@gmail.com>
2025-03-10 16:56:03 +0300

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDatabase = async () => {
   try {
     const connection = await mongoose.connect(
-      "mongodb+srv://eskndr:eskndr21@cluster0.eefsa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+      "mongodb+srv://eskndr:D4n5mOBtAagzlb1i@cluster0.eefsa.mongodb.net/book-store?retryWrites=true&w=majority&appName=Cluster0"
     );
     console.log("Database Connected");
   } catch (error) {
