4b25a124123a54e488dd26886490a02b16870d1b
Eskndr <eskndrytbarek323@gmail.com>
2025-03-10 16:42:51 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,10 @@ const cors = require("cors");
 const PORT = 5001;
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+}));
 app.use(express.json());
 
 connectDatabase();
